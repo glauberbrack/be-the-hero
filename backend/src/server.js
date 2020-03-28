d6cada3eebb4fff5afe3e25db0b8326928c72546
Glauber Brack <glauber@brack.com.br>
2020-03-28 21:03:44 +0000

    frotend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,3 @@
-import cors from 'cors';
 import app from './app';
 
-app.use(cors());
 app.listen(3333);
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <>
+      <Routes />
+    </>
+  );
+}
